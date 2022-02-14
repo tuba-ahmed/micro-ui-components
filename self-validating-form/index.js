6a880a73b12b684fe16ef4927e0e21641b631330
Tuba Ahmed <md.tuba.ahmed@gmail.com>
2022-02-14 11:23:57 -0800

    Adding styles to form and validation for email

--- self-validating-form/index.js
+++ self-validating-form/index.js
@@ -1,21 +1,78 @@
-function isValidName(name) {
+class ValidationError extends Error {
+	constructor(message) {
+		super();
+		this.message = message;
+	}
+}
+
+function validateName(name) {
 	const nameRegex = /^[a-zA-Z]+$/;
-	return name.test(nameRegex);
+	if (!nameRegex.test(name)) {
+		throw new ValidationError('Please enter a valid name.');
+	}
 }
 
-function isValidUsername(username) {
-	const usernameRegex = /^[a-zA-Z0-9._]+$/
-	return username.test(usernameRegex);
+function validateUsername(username) {
+	const usernameRegex = /^[a-zA-Z0-9._]+$/;
+	if (!usernameRegex.test(username)) {
+		throw new ValidationError('Please enter a valid username.');
+	}
 }
 
-function isValidBirthDay(day) {
-    const birthdayRegex = /^[0-9]{2}$/
-    return day.test(birthdayRegex);
+function validateDay(day) {
+	const birthdayRegex = /^[0-9]{2}$/;
+	if (!birthdayRegex.test(day)) {
+		throw new ValidationError('Please enter a valid Birthday.');
+	}
 }
 
-function isValidBirthYear(year) {
-    const birthYearRegex = /^[0-9]{4}$/
-    return year.test(birthYearRegex);
+function validateYear(year) {
+	const birthYearRegex = /^[0-9]{4}$/;
+	if (!birthYearRegex.test(year)) {
+		throw new ValidationError('Please enter a valid Birthyear.');
+	}
+}
+
+function validateEmail(email) {
+	const emailRegex = /^[a-zA-Z0-9]{1}[a-zA-Z0-9@._-]+[a-zA-Z]$/;
+	if (!emailRegex.test(email)) {
+		throw new ValidationError('Please enter a valid email');
+	}
+	const necessaryEmailCharacters = ['@', '.'];
+	for (const necessaryEmailCharacter of necessaryEmailCharacters) {
+		if (!email.includes(necessaryEmailCharacter)) {
+			throw new ValidationError('Please enter a valid email');
+		}
+	}
+}
+
+function validatePhoneNumber(phoneNumber) {
+	const FORMATTING_CHARACTERS = ['(', ')', '-'];
+
+	function validateFormattedNumber() {
+		const regex = /^[0-9(]{1}[0-9)-]+[0-9]$/;
+		const hasOpeningParentheses = phoneNumber.includes('(');
+		const hasClosingParentheses = phoneNumber.includes(')');
+		if (hasOpeningParentheses && !hasClosingParentheses) {
+			throw new ValidationError('Phone number missing closing parentheses');
+		}
+		if (!regex.test(phoneNumber)) {
+			throw new ValidationError('Please enter valid phone number');
+		}
+	}
+
+	function validateNonformattedNumber() {
+		const regex = /^[0-9]+$/;
+		if (!regex.test(phoneNumber)) {
+			throw new ValidationError('Please enter valid phone number');
+		}
+	}
+	for (const formattingCharacter of FORMATTING_CHARACTERS) {
+		if (phoneNumber.includes(formattingCharacter)) {
+			return validateFormattedNumber();
+		}
+	}
+	validateNonformattedNumber();
 }
 
 const PASSWORD_CATEGORIES = {
@@ -48,12 +105,34 @@ function getPasswordCategory(password) {
 	return PASSWORD_CATEGORIES.WEAK;
 }
 
+const validationMapping = {
+	'name': validateName,
+	'email': validateEmail,
+	'username': validateUsername,
+	'day': validateDay,
+	'year': validateYear,
+	'phoneNumber': validatePhoneNumber
+}
+
+
 function validate(event) {
 	const inputElement = event.target;
-	inputElement.classList.add('signup__field__input--error');
-
+	const field = inputElement.dataset.field;
+	if (field === 'password') {
+		return;
+	}
 	const errorMessageElement = event.target.parentElement.getElementsByClassName('signup__field__error')[0];
-	errorMessageElement.innerHTML = 'Sample Error';
+	try {
+		validationMapping[field](inputElement.value);
+		errorMessageElement.innerHTML = '';
+		inputElement.classList.remove('signup__field__input--error');
+	} catch (err) {
+		if (!(err instanceof ValidationError)) {
+			throw err;
+		}
+		errorMessageElement.innerHTML = err.message;
+		inputElement.classList.add('signup__field__input--error');
+	}
 }
 
 const inputs = document.getElementsByClassName('signup__field__input');
